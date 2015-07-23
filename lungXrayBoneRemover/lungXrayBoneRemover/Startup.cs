ad926f57909165e33efe58cd51e023789d7157d6
Dionata da Silva Nunes <dionata-nunes@hotmail.com>
2015-07-23 15:55:54 -0300

    Commit inicial

---
+++ lungXrayBoneRemover/lungXrayBoneRemover/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(lungXrayBoneRemover.Startup))]
+namespace lungXrayBoneRemover
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
